927186f576b1b6ff7e49aa3481ffc9eaf6c98f1e
LMMarshall <morwennamarshall@gmail.com>
2016-11-29 09:34:12 -0800

    Add update of existing file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions to create and cache matrices and
+## their inverses, then retrieve them from cache.
 
-## Write a short comment describing this function
+## Defines a square matrix, taking 
+## (x=matrix(numeric vector, nrow, ncol)) as an arg and
+## outputs a list to be used by cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    ## Check to see if the input matrix is square
+    entries <- length(c(x))
+    if(sqrt(entries) != sqrt(as.integer(entries))){
+        message("Error: matrix is not square")
+        break
+    }
+    
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set=set, get=get, 
+         setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## Takes a makeCacheMatrix object. Returns & stores
+## that object's inverse. If inv was previously
+## storied, it pulls from memory
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    ## Return a matrix that is the inverse of 'x'
+    inv
 }
